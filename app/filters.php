fba84ccc712f246066a4b83ad37df4c8c61570dd
jon-michael-c <jonmichaelcubano@gmail.com>
2024-10-22 12:25:13 -0400

    changes

--- app/filters.php
+++ app/filters.php
@@ -5,6 +5,8 @@
  */
 
 namespace App;
+use Illuminate\Support\Str;
+
 
 /**
  * Add "… Continued" to the excerpt.
@@ -24,3 +26,21 @@ add_filter('theme_page_templates', function ($templates) {
     $templates['template-legal.blade.php'] = __('Legal Template');
     return $templates;
 });
+
+/**
+ * Add `rel="preload"` to font files found in the manifest
+ */
+add_filter('wp_head', function (): void {
+    echo collect(
+        json_decode(asset('manifest.json')->contents())
+    )->keys()->filter(function ($item) {
+        return Str::endsWith($item, ['.otf', '.eot', '.woff', '.woff2', '.ttf']);
+    })->map(function ($item) {
+        return sprintf(
+            '<link rel="preload" href="%s" as="font" crossorigin>',
+            asset($item)->uri()
+        );
+    })->implode("\n");
+});
+
+
